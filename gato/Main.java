c9e5d586171f1b326b9c6bb5b6296e196b9ef9b4
Gabriel Santiago Rojas Leon <87994869+GabrielRojas74@users.noreply.github.com>
2022-03-08 09:02:29 -0500

    Update Main.java

--- gato/Main.java
+++ gato/Main.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 public class Main {
-  public static Scanner info= new Scanner (System.in);
-  public static Scanner info2= new Scanner (System.in);
+  public static Scanner tod= new Scanner (System.in);
+  public static Scanner stri= new Scanner (System.in);
   public static void main (String[]args){
     String nombre, color;
     int energia, comida, x, y, horas, juego;
-    System.out.println("Bienvenido al tamagochi ean");
+    System.out.println("Bienvenido al tamagochi EAN");
     
     System.out.println("Cual es el nombre del gato?: ");
-    nombre= info2.nextLine();
+    nombre= stri.nextLine();
     System.out.println("Cual es el color de "+nombre+"?");
-    color= info2.nextLine();
+    color= stri.nextLine();
     System.out.println("Cuanta energia tiene "+nombre+"?");
-    energia= info.nextInt();
+    energia= tod.nextInt();
     
     System.out.println("Cual es el nivel de alimento de "+nombre+"?");
-    comida= info.nextInt();
+    comida= tod.nextInt();
     
     System.out.println("·Que quieres hacer con "+nombre+"?"+"\n"+"·1 alimentarlo"+"\n"+ "·2 jugar "+"\n"+"·3 dormir"+"\n"+"·4 salir");
-    y= info.nextInt();  
+    y= tod.nextInt();  
 
  // Creación del objeto Paco que pertenece a la clase Gato (Instancia)
     Gato Paco= new Gato(nombre, color, energia, comida);
@@ -26,20 +26,21 @@ public class Main {
       switch(y){
         case 1: {
         System.out.println("·Cuanta comida quiere darle a "+nombre+"?");
-        x= info.nextInt();
+        x= tod.nextInt();
         Paco.comer(x);
         break;
           }
         case 2: {
-        System.out.println("·Cuantas veces quieres jugar con "+nombre+"?");
-        juego= info.nextInt();
         
+        System.out.println("·Cuantas veces quieres jugar con "+nombre+"?");
+        juego= tod.nextInt();
+        for (int i=0; i<juego;i++){
         Paco.jugar();
-          
+        break;}
         break;}
         case 3: {
         System.out.println("\n"+"·Cuantas horas va a dormir  "+nombre+"?"+" (de 1 a 5 horas)");
-        horas=info.nextInt();
+        horas=tod.nextInt();
         Paco.dormir(horas);       
         break;}
         case 4:{
@@ -51,7 +52,7 @@ public class Main {
           
     
      
-     } // fin del while true
+     } 
     
     } 
     
